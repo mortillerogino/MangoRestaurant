366dae2036c01fbbc652208e8a01a483a7818332
Gino Mortillero <mortillerogino@gmail.com>
2022-11-29 15:06:24 +0800

    Section 3: 32. Base Service Interface and API Request Model

---
+++ Mango.Web/Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto ResponseModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
