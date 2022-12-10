dd0e3a9673f7b3549c1127e8fc52373a71bd6121
Gino Mortillero <mortillerogino@gmail.com>
2022-12-10 16:00:38 +0800

    Section 4: 42: Update Product

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -28,6 +28,7 @@ namespace Mango.Web.Controllers
 
             return View(list);
         }
+
         public async Task<IActionResult> ProductCreate()
         {
             return View();
@@ -40,6 +41,35 @@ namespace Mango.Web.Controllers
             if (ModelState.IsValid)
             {
                 var response = await _productService.CreateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            var response = await _productService.GetProductAsync<ResponseDto>(productId);
+
+            if (response != null && response.IsSuccess)
+            {
+                ProductDto model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductEdit(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProductAsync<ResponseDto>(model);
                 if (response != null && response.IsSuccess)
                 {
                     return RedirectToAction(nameof(ProductIndex));
